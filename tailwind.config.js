278c3763bb60fa223b703ee5fd7957768a5e12c0
Bleu Rubin <bleurubin@protonmail.com>
2025-11-03 03:52:48 -0500

    remove unused class

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,6 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      animation: {
-        shimmer: 'shimmer 2s infinite',
-      },
-      keyframes: {
-        shimmer: {
-          '0%': { backgroundPosition: '-200% 0' },
-          '100%': { backgroundPosition: '200% 0' },
-        },
-      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
